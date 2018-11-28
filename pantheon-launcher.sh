78dbedde2bf664cd32f58bc80257a725e135aab4
Ismael marijuan <ismael.marijuan@biko2.com>
2018-11-28 22:51:33 +0100

    pantheon script

---
+++ pantheon-launcher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 1 - Crear sitio pantheon
+# 2 - descargar en local
+# 3 - ejecutar launcher
+# 4 - descargar docker con settings.local.php
+# 5 - mover docker a private
+# 6 - averiguar variables entorno y cambiar
+# 7 - levantar docker
+# 8 - entrar bash docker
+# 9 - importar bd --  `drush sql-connect` < example.sql
+# 10 - borrar caches, cron y watchdog
+# 11 - abrir ventana sitio
+# 12 - instalar dependencias tema y compilar
+# 13 - abrir terminal bash
+
+echo "llamada con argumento"
+read arg1
+echo arg1
