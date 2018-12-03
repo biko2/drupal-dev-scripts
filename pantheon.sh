8d99e4a5dc68cc012ee43e432b9048b0a22cc25e
chismen <45356643+chismen@users.noreply.github.com>
2018-12-03 15:37:42 +0100

    Update pantheon.sh

--- pantheon.sh
+++ pantheon.sh
@@ -81,7 +81,7 @@ fi
 # Editamos el archivo settings.local.php
 cd $RUTA/sites/default
 HOST=$PROYECTO'_mysql_1'
-echo 'host: '$HOST
+echo host: $HOST
 sed -i 's/docker/'"$PROYECTO"'/g' "settings.local.php"
 sed -i 's/localhost_bd/'"$HOST"'/g' "settings.local.php"
 
